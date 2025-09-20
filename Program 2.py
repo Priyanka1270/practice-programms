d9f0cea3e0daf3bc262596794624c55fafd0cdac
Priyanka1270 <priyankadubakula476@gmail.com>
2025-09-20 14:36:26 +0530

    Add files via upload

---
+++ Program 2.py
@@ -0,0 +1,11 @@
+#print Arthmatic operations on addition and division
+
+Num1=int(input("Enter the number: "))
+Num2=int(input("Enter the number: "))
+sum_result=Num1+Num2
+print(sum_result)
+
+N1 = 52
+N2 = 90
+division=N1/N2
+print(division)
